40db6159d9ca3c89ebf2de0b7e03ffdec97fe20c
Masahiro Sato <masahiro.satoo@gmail.com>
2025-04-08 14:28:33 +0900

    App.jsを更新して認証とルーティングを追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Header from './components/common/Header';
 import TodoApp from './components/TodoApp';
+import AuthContainer from './components/auth/AuthContainer';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <TodoApp />
-    </div>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="app-container">
+            <Routes>
+              {/* トップページ (ログイン済みならTODOアプリにリダイレクト、未ログインならログインページへ) */}
+              <Route 
+                path="/" 
+                element={
+                  <ProtectedRoute>
+                    <TodoApp />
+                  </ProtectedRoute>
+                } 
+              />
+              
+              {/* ログインページ */}
+              <Route path="/login" element={<AuthContainer />} />
+              
+              {/* リセットパスワードページ (tokenを含むURLのため) */}
+              <Route path="/reset-password" element={<AuthContainer />} />
+              
+              {/* 不明なパスは全てトップページにリダイレクト */}
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
