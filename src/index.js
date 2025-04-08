edc619437ff02fa00b1e54a81460183262d64588
Masahiro Sato <masahiro.satoo@gmail.com>
2025-04-08 14:16:12 +0900

    srcディレクトリとindex.jsの追加

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// パフォーマンス計測が必要な場合はコメントを外す
+// reportWebVitals(console.log);
+reportWebVitals();
